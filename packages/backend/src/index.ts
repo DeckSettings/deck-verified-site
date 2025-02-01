709c5aebaa2d6a27fa14f04d33dfcf05564f0df4
Josh.5 <jsunnex@gmail.com>
2025-02-01 19:22:51 +1300

    FIx location of index page for siteindex.xml

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -508,7 +508,7 @@ app.get('/sitemap.xml', async (req: Request, res: Response) => {
     const baseUrl = `${proto}://${req.get('host')}`
     const lastModDate = format(new Date(), 'yyyy-MM-dd') // Current date for lastmod
     const staticPages = [
-      { loc: `${baseUrl}/`, priority: '1.0' },
+      { loc: `${baseUrl}/deck-verified/`, priority: '1.0' },
       { loc: `${baseUrl}/deck-verified/games-with-reports`, priority: '0.8' }
     ]
 
