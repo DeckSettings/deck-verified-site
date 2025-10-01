cbe271e102febafe26b891e3b2f6337a380f5aeb
Josh.5 <jsunnex@gmail.com>
2025-10-02 12:03:49 +1300

    Fix SSR builds

--- packages/frontend/src/boot/platform-check.ts
+++ packages/frontend/src/boot/platform-check.ts
@@ -2,6 +2,14 @@ import { boot } from 'quasar/wrappers'
 import { Platform } from 'quasar'
 
 export default boot(() => {
+  if (process.env.SERVER) {
+    globalThis.isCapacitor = false
+    globalThis.isSsr = true
+    globalThis.isMobile = process.env.BUILD_TARGET === 'mobile'
+    return
+  }
+
+  // App is not running on server, we can safely use Platform
   globalThis.isCapacitor = 'capacitor' in Platform.is ? (Platform.is as { capacitor: boolean }).capacitor : false
   globalThis.isSsr = 'ssr' in Platform.is ? (Platform.is as { ssr: boolean }).ssr : false
   globalThis.isMobile = globalThis.isCapacitor || (process.env.BUILD_TARGET === 'mobile')
