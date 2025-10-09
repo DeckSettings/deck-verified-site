6f1ba8c22df07960d2d1df1271d8f4eaa92d3970
Josh.5 <jsunnex@gmail.com>
2025-10-09 20:03:10 +1300

    Enable login feature by default

--- packages/frontend/src/composables/useFeatureFlags.ts
+++ packages/frontend/src/composables/useFeatureFlags.ts
@@ -1,6 +1,6 @@
 // Provide a list of feature flags
 export const featureFlags = {
-  enableLogin: import.meta.env.VITE_ENABLE_LOGIN === 'true',
+  enableLogin: import.meta.env.VITE_ENABLE_LOGIN !== 'false',
 }
 
 export const useFeatureFlags = () => featureFlags
