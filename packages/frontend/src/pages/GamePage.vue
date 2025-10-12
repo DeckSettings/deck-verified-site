6a6e5ec72d72d9615128587b83feff8a084bd542
Josh.5 <jsunnex@gmail.com>
2025-10-13 12:46:49 +1300

    Add pull to refresh on game page

--- packages/frontend/src/pages/GamePage.vue
+++ packages/frontend/src/pages/GamePage.vue
@@ -4,6 +4,7 @@ import ScrollToTop from 'components/elements/ScrollToTop.vue'
 import { useGameStore } from 'src/stores/game-store'
 import type { RouteLocationNormalizedLoaded } from 'vue-router'
 import type { Pinia } from 'pinia'
+import { nextTick, ref } from 'vue'
 
 // Quasar preFetch (SSR + client) to ensure game data is loaded before render
 defineOptions({
@@ -19,13 +20,22 @@ defineOptions({
     }
   },
 })
+
+const refreshVersion = ref(0)
+const handleRefresh = async (done: () => void) => {
+  refreshVersion.value++
+  await nextTick()
+  done()
+}
 </script>
 
 <template>
-  <q-page class="bg-dark text-white q-pb-xl" :padding="!$q.platform.isMobileUi">
-    <GameData />
-    <ScrollToTop />
-  </q-page>
+  <q-pull-to-refresh class="fit" no-mouse @refresh="handleRefresh">
+    <q-page class="bg-dark text-white q-pb-xl" :padding="!$q.platform.isMobileUi">
+      <GameData :key="`GameData-${refreshVersion}`" />
+      <ScrollToTop />
+    </q-page>
+  </q-pull-to-refresh>
 </template>
 
 <style scoped>
