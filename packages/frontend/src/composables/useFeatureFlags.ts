f24b5e6e28c551b106e96e0e065e87709bfab6a7
Josh.5 <jsunnex@gmail.com>
2025-10-28 22:20:45 +1300

    Enable mobile link on index page for all builds

--- packages/frontend/src/composables/useFeatureFlags.ts
+++ packages/frontend/src/composables/useFeatureFlags.ts
@@ -2,7 +2,7 @@
 export const featureFlags = {
   enableLogin: import.meta.env.VITE_ENABLE_LOGIN !== 'false',
   enableHelpOverlay: import.meta.env.VITE_ENABLE_HELP_OVERLAY === 'true',
-  enableMobileAppLink: import.meta.env.VITE_MOBILE_APP_LINK === 'true',
+  enableMobileAppLink: import.meta.env.VITE_MOBILE_APP_LINK !== 'false',
 }
 
 export const useFeatureFlags = () => featureFlags
